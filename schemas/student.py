926498de9afeac8868096e5499cdb5c145ccf1a4
Yuk1n3IzHav <tmzerogaming@gmail.com>
2026-07-17 08:16:53 +0700

    Update student.py

--- schemas/student.py
+++ schemas/student.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field
 
 from schemas.classroom import ClassroomResponse
 
@@ -6,7 +6,7 @@ from schemas.classroom import ClassroomResponse
 class StudentCreate(BaseModel):
     student_code: str = Field(..., min_length=3, max_length=20)
     full_name: str = Field(..., min_length=2, max_length=50)
-    email: EmailStr
+    email: str = Field(..., min_length=2, max_length=50)
     class_id: int = Field(..., ge=1)
 
 
